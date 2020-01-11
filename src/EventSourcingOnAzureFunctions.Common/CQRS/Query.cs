8ec9e764ad9cf40437e3a46015b64ece486bcff1
MerrionComputing <Duncan@merrioncomputing.com>
2020-01-11 23:09:26 +0000

    Query event stream backing

--- src/EventSourcingOnAzureFunctions.Common/CQRS/Query.cs
+++ src/EventSourcingOnAzureFunctions.Common/CQRS/Query.cs
@@ -1,7 +1,13 @@
 ﻿using EventSourcingOnAzureFunctions.Common.Binding;
+using EventSourcingOnAzureFunctions.Common.CQRS.Common.Events;
+using EventSourcingOnAzureFunctions.Common.CQRS.QueryHandler.Events;
+using EventSourcingOnAzureFunctions.Common.EventSourcing;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Implementation;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EventSourcingOnAzureFunctions.Common.CQRS
 {
@@ -10,6 +16,8 @@ namespace EventSourcingOnAzureFunctions.Common.CQRS
     /// </summary>
     public class Query
     {
+
+        private readonly IWriteContext _queryContext;
 
         private readonly string _domainName;
         /// <summary>
@@ -47,6 +55,94 @@ namespace EventSourcingOnAzureFunctions.Common.CQRS
             }
         }
 
+        // Parameters
+        public async Task SetParameter(string parameterName, object parameterValue)
+        {
+            if (!string.IsNullOrWhiteSpace(parameterName))
+            {
+
+                EventStream esQry = new EventStream(new EventStreamAttribute(MakeDomainQueryName(DomainName),
+                    QueryName,
+                    UniqueIdentifier),
+                    context: _queryContext);
+
+                ParameterValueSet evParam = new ParameterValueSet()
+                {
+                    Name = parameterName,
+                    Value = parameterValue
+                };
+
+                await esQry.AppendEvent(evParam);
+            }
+        }
+
+        // Validations
+
+        // Classification request
+
+        // Projection request
+
+        // Collations
+
+        // Response
+        private async Task SetResponseTarget(string targetType,
+            string targetLocation)
+        {
+            if ((!string.IsNullOrWhiteSpace(targetType )) && (!string.IsNullOrWhiteSpace(targetLocation )))
+            {
+
+                EventStream esQry = new EventStream(new EventStreamAttribute(MakeDomainQueryName(DomainName),
+                    QueryName,
+                    UniqueIdentifier),
+                    context: _queryContext);
+
+                OutputLocationSet evParam = new OutputLocationSet()
+                {
+                    TargetType  = targetType ,
+                    Location  = targetLocation 
+                };
+
+                await esQry.AppendEvent(evParam);
+            }
+        }
+
+        /// <summary>
+        /// Add an azure storage blob target for the query to save its results
+        /// into
+        /// </summary>
+        public async Task AddStorageBlobOutput(Uri containerAddress,
+            Uri targetFilename)
+        {
+            await SetResponseTarget("Azure Storage Blob",
+                new Uri(containerAddress, targetFilename).AbsoluteUri); 
+        }
+
+        /// <summary>
+        /// Add an email output target for the query results
+        /// </summary>
+        /// <param name="emailAddress">
+        /// The address to which to send the query results
+        /// </param>
+        public async Task AddEmailOutput(string emailAddress)
+        {
+            await SetResponseTarget("Email Address",
+                emailAddress );
+        }
+
+        /// <summary>
+        /// Add a webhook output target for the query results
+        /// </summary>
+        /// <param name="webhookAddress">
+        /// The URI of the web hook to which to post results
+        /// </param>
+        public async Task AddWebhookOutput(Uri webhookAddress)
+        {
+            await SetResponseTarget("Webhook",
+                webhookAddress.AbsoluteUri );
+        }
+        
+
+
         public Query(QueryAttribute attribute)
         {
             if (null != attribute )
@@ -54,6 +150,12 @@ namespace EventSourcingOnAzureFunctions.Common.CQRS
                 _domainName = attribute.DomainName;
                 _queryName = attribute.QueryName;
                 _uniqueIdentifier = attribute.UniqueIdentifier;
+                // Make a query context
+                _queryContext = new WriteContext()
+                {
+                    Source = _queryName ,
+                    CausationIdentifier = _uniqueIdentifier
+                };
             }
         }
 
