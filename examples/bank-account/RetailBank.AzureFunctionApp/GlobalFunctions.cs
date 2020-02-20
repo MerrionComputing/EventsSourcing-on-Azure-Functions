f4d9d5993de985c5532fe18a9bbbf9337d73af58
MerrionComputing <Duncan@merrioncomputing.com>
2020-02-20 19:51:56 +0000

    Added SignalR output to EventGridEcho for demo purposes

--- examples/bank-account/RetailBank.AzureFunctionApp/GlobalFunctions.cs
+++ examples/bank-account/RetailBank.AzureFunctionApp/GlobalFunctions.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.Azure.EventGrid.Models;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RetailBank.AzureFunctionApp
 {
@@ -21,19 +25,41 @@ namespace RetailBank.AzureFunctionApp
         /// <param name="eventGridEvent"></param>
         /// <param name="log"></param>
         [FunctionName(nameof(EventGridEcho))]
-        public static void EventGridEcho([EventGridTrigger]EventGridEvent eventGridEvent, 
-            ILogger log)
+        public static Task EventGridEcho([EventGridTrigger]EventGridEvent eventGridEvent, 
+            ILogger log,
+            [SignalR(HubName = "retailbank")]IAsyncCollector<SignalRMessage> signalRMessages)
         {
             if (null != eventGridEvent)
             {
                 log.LogInformation($"{eventGridEvent.EventType} :: {eventGridEvent.Subject}");
                 log.LogInformation(eventGridEvent.Data.ToString());
+
+                return signalRMessages.AddAsync(
+                    new SignalRMessage
+                    {
+                        Target = eventGridEvent.EventType,
+                        Arguments = new[] { eventGridEvent.Data }
+                    });
+
             }
             else
             {
                 log.LogError($"Event grid event is null");
+                return Task.CompletedTask; 
             }
         }
 
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+               [HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
+               [SignalRConnectionInfo
+                   (HubName = "retailbank", 
+                    UserId = "{headers.x-ms-client-principal-id}")]
+               SignalRConnectionInfo connectionInfo)
+        {
+            // connectionInfo contains an access key token with a name identifier claim set to the authenticated user
+            return connectionInfo;
+        }
     }
 }
