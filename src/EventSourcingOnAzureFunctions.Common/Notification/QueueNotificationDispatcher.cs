b8fe73004148efdef0fc0e9b7a725beb74340bab
MerrionComputing <Duncan@merrioncomputing.com>
2020-10-21 20:53:53 +0100

    Queue notification - generate queue name from stream identifier

--- src/EventSourcingOnAzureFunctions.Common/Notification/QueueNotificationDispatcher.cs
+++ src/EventSourcingOnAzureFunctions.Common/Notification/QueueNotificationDispatcher.cs
@@ -2,8 +2,9 @@
 using EventSourcingOnAzureFunctions.Common.EventSourcing.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Azure.Storage.Queues;
 using System.Threading.Tasks;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Implementation.AzureStorage.AppendBlob;
 
 namespace EventSourcingOnAzureFunctions.Common.Notification
 {
@@ -25,29 +26,75 @@ namespace EventSourcingOnAzureFunctions.Common.Notification
         public const string QUEUE_COMMAND_PROJECTIONS = "command-projection-requests";
         public const string QUEUE_COMMAND_CLASSIFICATIONS = "query-classification-requests";
 
-        public Task ExistingEntityDeleted(IEventStreamIdentity deletedEntity, string commentary = "", IWriteContext context = null)
+
+        public Task NewEntityCreated(IEventStreamIdentity newEntity, 
+            string commentary = "", 
+            IWriteContext context = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task NewEntityCreated(IEventStreamIdentity newEntity, string commentary = "", IWriteContext context = null)
+        public Task NewEventAppended(IEventStreamIdentity targetEntity, 
+            string eventType, 
+            int sequenceNumber, 
+            string commentary = "", 
+            object eventPayload = null, 
+            IWriteContext context = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task NewEventAppended(IEventStreamIdentity targetEntity, string eventType, int sequenceNumber, string commentary = "", object eventPayload = null, IWriteContext context = null)
+        public Task ExistingEntityDeleted(IEventStreamIdentity deletedEntity, 
+            string commentary = "", 
+            IWriteContext context = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task ProjectionCompleted(IEventStreamIdentity targetEntity, string projectionType, int asOfSequenceNumber, DateTime? asOfDate, object currentValue, string commentary = "")
+
+
+
+
+        public Task ProjectionCompleted(IEventStreamIdentity targetEntity, 
+            string projectionType, 
+            int asOfSequenceNumber, 
+            DateTime? asOfDate, 
+            object currentValue, 
+            string commentary = "")
         {
             throw new NotImplementedException();
         }
 
-        public Task ClassificationCompleted(IEventStreamIdentity targetEntity, string classificationType, Dictionary<string, object> parameters, int asOfSequenceNumber, DateTime? asOfDate, ClassificationResponse response, string commentary = "")
+        public Task ClassificationCompleted(IEventStreamIdentity targetEntity, 
+            string classificationType, 
+            Dictionary<string, object> parameters, 
+            int asOfSequenceNumber, 
+            DateTime? asOfDate, 
+            ClassificationResponse response, 
+            string commentary = "")
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Turn the identity of an event stream backed entity into the name of a 
+        /// queue down which the notifications about that entity should be posted
+        /// </summary>
+        /// <param name="targetEntity">
+        /// The entity for which a notification has occurred
+        /// </param>
+        /// <returns></returns>
+        public static string MakeQueueName(IEventStreamIdentity targetEntity)
+        {
+            if (targetEntity != null)
+            {
+                string basename= BlobEventStreamBase.MakeValidStorageFolderName($"{targetEntity.DomainName}-{targetEntity.EntityTypeName}");
+                // Queues also cannot start with a number
+                return basename.TrimStart(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+            }
+
+            // The target that applies if the entity cannoy be identified
+            return $"unidentified-entities";
         }
     }
 }
---
+++ src/EventSourcingOnAzureFunctions.Test/QueueNotificationDispatcher_UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using EventSourcingOnAzureFunctions.Common.Binding;
+using EventSourcingOnAzureFunctions.Common.CQRS;
+using EventSourcingOnAzureFunctions.Common.CQRS.ClassifierHandler.Functions;
+using EventSourcingOnAzureFunctions.Common.EventSourcing;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Implementation;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Interfaces;
+using EventSourcingOnAzureFunctions.Common.Notification;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace EventSourcingOnAzureFunctions.Test
+{
+    [TestClass]
+    public class QueueNotificationDispatcher_UnitTest
+    {
+
+        [TestMethod]
+        public void MakeQueueName_Valid_TestMethod()
+        {
+            string expected = "bank-account";
+            string actual = "not set";
+
+            IEventStreamIdentity target = new EventStreamAttribute("Bank", "Account", "J920377-A");
+            actual = QueueNotificationDispatcher.MakeQueueName(target); 
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MakeQueueName_Inalid_TestMethod()
+        {
+            string expected = "bank-account";
+            string actual = "not set";
+
+            IEventStreamIdentity target = new EventStreamAttribute("1Bank", "Account-", "J920377-A");
+            actual = QueueNotificationDispatcher.MakeQueueName(target);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
