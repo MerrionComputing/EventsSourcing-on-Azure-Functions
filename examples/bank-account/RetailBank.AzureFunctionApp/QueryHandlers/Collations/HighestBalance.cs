13fa1bd469b31091df3eae1454fb5b62898d3e5e
Duncan Jones <duncan.jones@hedgefacts.com>
2021-11-06 12:33:47 +0000

    Highest Balance Collation code (experimental)

---
+++ examples/bank-account/RetailBank.AzureFunctionApp/QueryHandlers/Collations/HighestBalance.cs
@@ -0,0 +1,21 @@
+﻿using EventSourcingOnAzureFunctions.Common.EventSourcing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailBank.AzureFunctionApp.QueryHandlers.Collations
+{
+    /// <summary>
+    /// The account that has the highest balance in a given query result set
+    /// </summary>
+    public sealed class HighestBalance
+    {
+
+        string AccountNumber { get; set; }
+
+        Nullable<decimal> CurrentBalance { get; set; }
+
+        int AsOfSequenceNumber { get; set; }
+
+    }
+}
---
+++ examples/bank-account/RetailBank.AzureFunctionApp/QueryHandlers/Collations/HighestBalanceQueryCollation.cs
@@ -0,0 +1,28 @@
+﻿using EventSourcingOnAzureFunctions.Common.CQRS.ProjectionHandler.Events;
+using EventSourcingOnAzureFunctions.Common.EventSourcing;
+using EventSourcingOnAzureFunctions.Common.EventSourcing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailBank.AzureFunctionApp.QueryHandlers.Collations
+{
+    /// <summary>
+    /// A projection to return the account with the highest balance in a query 
+    /// </summary>
+    [ProjectionName("Highest Balance Collation")]
+    public sealed class HighestBalanceQueryCollation
+        : ProjectionBase,
+        IHandleEventType<ProjectionValueReturned>
+    {
+
+        public void HandleEventInstance(ProjectionValueReturned eventInstance)
+        {
+            if (eventInstance != null)
+            {
+                // if the projection was "Get Balance" for an account...
+
+            }
+        }
+    }
+}
